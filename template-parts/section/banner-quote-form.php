44d131d9223c5d70b9d841e91caacdc6c27840f3
IrinaChuk <ira.matvichuk@mail.ru>
2026-04-28 18:16:43 +0300

    Обновлена разметка с выводом полей для баннера расчета

--- template-parts/section/banner-quote-form.php
+++ template-parts/section/banner-quote-form.php
@@ -1,7 +1,17 @@
 <?php 
   $page_id = $args["id"];
   $sec_name = $args["name"]["value"];
+
+  // Получаем поля из админки
+  $title = get_field($sec_name . '_title', $page_id);
+  $image = get_field($sec_name . '_image', $page_id);
+  $form_shortcode = get_field($sec_name . '_form', $page_id);
+
+  // Значения по умолчанию (если поля не заполнены)
+$title = !empty($title) ? $title : 'Рассчитаем стоимость вашего автомобиля';
+$form_shortcode = !empty($form_shortcode) ? $form_shortcode : '[contact-form-7 id="7d07704" title="Форма Запрос расчета"]';
 ?>
+
 
 <section class="banner-quote-form sec-offset">
   <div class="container">
@@ -12,43 +22,33 @@
         <!-- Обёртка для заголовка с фоном на всю ширину -->
         <div class="banner-quote-form__title-wrapper">
           <h2 class="banner-quote-form__title sec-title" data-aos="fade-up">
-            Рассчитаем стоимость вашего автомобиля
+           <?php echo esc_html($title); ?>
           </h2>
         </div>
 
-        <form class="banner-quote-form__form">
-          <div class="banner-quote-form__field">
-            <input type="tel" placeholder="Телефон" required />
-          </div>
-
-          <div class="banner-quote-form__field">
-            <input type="text" placeholder="Комментарий" />
-          </div>
-
-          <button class="banner-quote-form__btn" type="submit">
-            Получить расчет
-          </button>
-          <div class="banner-quote-form__checkbox">
-            <label>
-              <input type="checkbox" required />
-              <span class="banner-quote-form__checkbox-text">
-                Нажимая на кнопку, вы даёте согласие на обработку
-                персональных данных
-              </span>
-            </label>
-          </div>
-        </form>
+       <!-- Выводим форму через шорткод CF7 -->
+        <?php echo do_shortcode($form_shortcode); ?>
       </div>
 
       <!-- Правая часть с картинкой -->
-      <div class="banner-quote-form__image">
-        <img
-          class="banner-quote-form__img"
-          src="banner-image.png"
-          alt="Мужчина с деньгами"
-        />
-      </div>
-      <!-- </div> -->
+     <div class="banner-quote-form__image">
+        <?php if(!empty($image)) : ?>
+          <picture class="banner-quote-form__picture">
+            <?php if(!empty($image['webp_1x'])) : ?>
+              <source srcset="<?php echo esc_url($image['webp_1x']); ?>" type="image/webp">
+            <?php endif; ?>
+                <img 
+                  class="banner-quote-form__img"
+                  src="<?php echo esc_url($image['url']); ?>"
+                  alt="<?php echo esc_attr($image['alt']); ?>"
+                  width="412"
+                  height="405"
+                  loading="lazy"
+                />
+          </picture>
+        <?php endif; ?>
+    </div>
+  
     </div>
   </div>
 </section>
