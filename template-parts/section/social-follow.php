1f99fdd44594b53da923cd50f86201e1107be839
ИринаЧук <ira.matvichuk@mail.ru>
2026-05-05 17:34:14 +0300

    Готова секция 'Мы в соцсетях' с адаптивом на декстоп и мобилку, промежуточного нет пока

--- template-parts/section/social-follow.php
+++ template-parts/section/social-follow.php
@@ -1,5 +1,121 @@
 <?php 
-  $page_id = $args["id"];
-  $sec_name = $args["name"]["value"];
-
+$page_id = $args["id"];
+$sec_name = $args["name"]["value"];
 ?>
+
+<section class="social-follow sec-offset">
+  <div class="container">
+      <div class="social-follow__wrap">
+        <div class="social-follow__content">
+          <div class="social-follow__content-up">
+            <h2 class="social-follow__title sec-title" data-aos="fade-up">
+              Мы в соцсетях
+            </h2>
+            <span class="social-follow__descr">
+              Подписывайтесь и узнавайте первыми о новостях компании
+            </span>
+          </div>
+
+          <div class="social-follow__left">
+            <div class="social-follow__image">
+              <picture class="social-follow__picture">
+                <img
+                  class="social-follow__img"
+                  src="/wp-content/themes/primasnab/img/banner-social-tel.png"
+                  alt="Телефон"
+                  width="619"
+                  height="619"
+                />
+              </picture>
+            </div>
+
+            <div class="social-follow__var">
+              <ul class="social-follow__messanger messanges" title="messanges">
+                <li class="messanges__item">
+                  <a href="https://t.me/primasnabavto" target="_blank" class="messanges__link" aria-label="Telegram">
+                  <img src="/wp-content/themes/primasnab/img/icon/Telegram-banner.svg" class="messanges__icon" width="95" height="96" alt="иконка Telegram">
+                  </a>
+                </li>
+                <li class="messanges__item">
+                  <a href="https://vk.com/primasnabavto" target="_blank" class="messanges__link" aria-label="VK">
+                  <img src="/wp-content/themes/primasnab/img/icon/VK-banner.svg" class="messanges__icon" width="95" height="96" alt="иконка VK">
+                  </a>
+                </li>
+                <li class="messanges__item">
+                  <a href="https://youtube.com/primasnabavto" target="_blank" class="messanges__link" aria-label="YouTube">
+                  <img src="/wp-content/themes/primasnab/img/icon/YouTube-banner.svg" class="messanges__icon" width="95" height="96" alt="иконка YouTube">
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <img
+          src="/wp-content/themes/primasnab/img/icon/icon-bell.svg" class="social-follow__left-icon" width="115" height="115" alt="иконка колокольчика"
+            class="social-follow__left-icon"
+            width="115"
+            height="115"
+            alt="иконка колокольчика"
+          />
+        </div>
+
+        <div class="social-follow__right">
+          <img
+            class="social-follow__right-image"
+            src="/wp-content/themes/primasnab/img/social-follow.png"
+            alt="Двое мужчин разговаривают"
+            width="780"
+            height="784"
+            loading="lazy"
+          />
+          <div class="social-follow__right-inner">
+            <img
+            src="/wp-content/themes/primasnab/img/icon/post_contact.svg" class="social-follow__right-icon" width="69" height="69" alt="иконка конверта"
+              class="social-follow__right-icon"
+              width="69"
+              height="69"
+              alt="иконка конверта"
+            />
+            <p class="social-follow__right-text">
+              Автоновости, обзоры на самые крутые авто, полезная
+              информация в наших социальных сетях
+            </p>
+          </div>
+        </div>
+      </div>
+
+        <div class="social-follow__right-inner social-follow__right-inner--mobile">
+            <img
+            src="/wp-content/themes/primasnab/img/icon/post_contact.svg" class="social-follow__right-icon" width="69" height="69" alt="иконка конверта"
+              class="social-follow__right-icon"
+              width="69"
+              height="69"
+              alt="иконка конверта"
+            />
+            <p class="social-follow__right-text">
+              Автоновости, обзоры на самые крутые авто, полезная
+              информация в наших социальных сетях
+            </p>
+          </div>
+
+        <div class="social-follow__var social-follow__var--mobile">
+              <ul class="social-follow__messanger messanges" title="messanges">
+                <li class="messanges__item">
+                  <a href="https://t.me/primasnabavto" target="_blank" class="messanges__link" aria-label="Telegram">
+                  <img src="/wp-content/themes/primasnab/img/icon/Telegram-banner.svg" class="messanges__icon" width="95" height="96" alt="иконка Telegram">
+                  </a>
+                </li>
+                <li class="messanges__item">
+                  <a href="https://vk.com/primasnabavto" target="_blank" class="messanges__link" aria-label="VK">
+                  <img src="/wp-content/themes/primasnab/img/icon/VK-banner.svg" class="messanges__icon" width="95" height="96" alt="иконка VK">
+                  </a>
+                </li>
+                <li class="messanges__item">
+                  <a href="https://youtube.com/primasnabavto" target="_blank" class="messanges__link" aria-label="YouTube">
+                  <img src="/wp-content/themes/primasnab/img/icon/YouTube-banner.svg" class="messanges__icon" width="95" height="96" alt="иконка YouTube">
+                  </a>
+                </li>
+              </ul>
+        </div>
+  </div>
+</section>
