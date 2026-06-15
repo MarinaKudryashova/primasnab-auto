d0ec3d1b76c30f49aa63cf4e250a9847c8ea1719
ИринаЧук <ira.matvichuk@mail.ru>
2026-06-15 16:31:51 +0300

    Общие стили для модалок

--- template-parts/components/modal.php
+++ template-parts/components/modal.php
@@ -1,9 +1,13 @@
 <div class="graph-modal">
   <div class="graph-modal__container" role="dialog" aria-modal="true" data-graph-target="modal-leadform">
     <button class="btn-reset js-modal-close graph-modal__close" aria-label="Закрыть модальное окно">
-      <svg>
+      <!-- <svg>
         <use xlink:href="<?php echo get_template_directory_uri();?>/img/sprite.svg#close"></use>
-      </svg>
+      </svg> -->
+       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+        <line x1="18" y1="6" x2="6" y2="18"></line>
+        <line x1="6" y1="6" x2="18" y2="18"></line>
+       </svg>
     </button>
     <div class="graph-modal__content form">
       <?php if( have_rows( 'modal_leadform', 'option') ): ?>
